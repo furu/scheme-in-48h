bec6bcf4e733f28c07b09a091d9a6b2662b7f470
furu <ba09219@gmail.com>
2016-01-20 01:22:16 +0900

    コマンドラインから名前を読み込み挨拶を表示する

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,11 @@
 module Main where
 
+import System.Environment
+
+-- 最初の Haskell プログラム
+-- コマンドラインから名前を読み込み挨拶を表示する
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  putStrLn ("Hello, " ++ args !! 0)
