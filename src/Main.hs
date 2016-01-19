7dfe8566921cbf4c6a355d8c9adfaf2f7c34134f
furu <ba09219@gmail.com>
2016-01-20 01:46:12 +0900

    練習問題1の1を解いた

--- src/Main.hs
+++ src/Main.hs
@@ -2,10 +2,15 @@ module Main where
 
 import System.Environment
 
--- 最初の Haskell プログラム
--- コマンドラインから名前を読み込み挨拶を表示する
+-- 練習問題1
+-- 1. コマンドラインから2つの引数を取り、
+-- その両方を使ってメッセージを出力するようにプログラムを変更しなさい。
+--
+-- 1番目の引数として挨拶の言葉を受け取り、
+-- 2番目の引数として名前を受け取り、
+-- {挨拶の言葉}, {名前} を表示するように変更した。
 
 main :: IO ()
 main = do
   args <- getArgs
-  putStrLn $ "Hello, " ++ head args
+  putStrLn $ (args !! 0) ++ ", " ++ (args !! 1)
