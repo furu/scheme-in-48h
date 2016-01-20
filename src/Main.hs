8c353dfe65ef080902e812015442d7bd81be1751
furu <ba09219@gmail.com>
2016-01-21 01:46:27 +0900

    行バッファリングされるらしい
    https://mail.haskell.org/pipermail/haskell/2006-September/018430.html

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import System.IO
+
 -- 練習問題1
 -- 3. getLineはコンソールから一行読み込み、文字列として返すIOアクションです。
 --    名前を入力を促し、名前を読み、コマンドライン引数の代わりにそれを出力する
@@ -8,5 +10,6 @@ module Main where
 main :: IO ()
 main = do
   putStr "put your name: "
+  hFlush stdout
   name <- getLine
   putStrLn $ "Hi, " ++ name
