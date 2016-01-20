0ed94bdfbc3fc4b06f973e7f5e630063f4851912
furu <ba09219@gmail.com>
2016-01-21 02:58:00 +0900

    戻り値

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,15 @@ module Main where
 -- spaces は後で自分で定義するため
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
+
+-- Lisp の値を表すデータ型
+data LispVal =   Atom String
+               | List [LispVal]
+               | DottedList [LispVal] LispVal
+               | Number Integer
+               | String String
+               | Bool Bool
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -15,8 +24,34 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+-- アトムは1つの文字か記号のあとに0個以上の文字、数字、または記号が連なったもの
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest  <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+             "#t" -> Bool True
+             "#f" -> Bool False
+             _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+-- <|> は1つ目のパーサを試し、それが失敗したら2つ目を試し、
+-- それも失敗したら3つ目を試し…、成功したパーサから返ってきた値を返す
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
+
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
                    Left err  -> "No match: " ++ show err
                    Right val -> "Found value"
 
